2197ffefae79eca136df6d193f66899e5e4bc802
talenaa <talenabarbosa7@gmail.com>
2025-03-20 21:16:10 +0100

    style: header and nav

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from './Header.jsx'
 import Food from './Food.jsx'
 import Footer from './Footer.jsx'
 
-function App() {
+function App(){
   
     return(
       <>
--- src/Food.jsx
+++ src/Food.jsx
@@ -1,13 +1,15 @@
-function Food() {
+function Food(){
     
     const food1 = "Pizza";
     const food2 = "Fries";
+    const food3 = "Tacos";
 
     return(
         <ul>
             <li>Apple</li>
             <li>{food1}</li>
             <li>{food2.toUpperCase()}</li>
+            <li>{food3.toLowerCase()}</li>
         </ul>
     );
 }
