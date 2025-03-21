dc6743d5120b38cba2e33ba81edda7194c8988fa
talenaa <talenabarbosa7@gmail.com>
2025-03-21 15:37:37 +0100

    feat: button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Header.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
 import Footer from './Footer.jsx'
 
 function App(){
@@ -10,6 +11,7 @@ function App(){
         <Header/>
         <Food/>
         <Card/>
+        <Button/>
         <Footer/>
       </>
     );
