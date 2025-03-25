7df2e0c837403cc1fb1ad90c5cb13845e0c3477c
talenaa <talenabarbosa7@gmail.com>
2025-03-25 14:32:03 +0100

    style: inline css style

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,17 @@
 function Footer(){
+
+    const styles = {
+        fontWeight: "bold",
+        color: "white",
+        marginTop: "auto",
+        textAlign: "center",
+        padding: "20px",
+        backgroundColor:"hsl(224, 40.40%, 22.40%)",
+        fontFamily: "arial",
+    }
+
     return(
-        <footer>
+        <footer style={styles}>
             <p>&copy; {new Date().getFullYear()} My website</p>
         </footer>
     );
