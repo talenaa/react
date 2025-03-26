1c463639f49a23739b5fde4735555fac33d5753a
talenaa <talenabarbosa7@gmail.com>
2025-03-26 13:26:29 +0100

    feat: props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Header.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
 import Button from './Button/Button.jsx'
+import Student from './Student.jsx'
 import Footer from './Footer.jsx'
 
 function App(){
@@ -12,6 +13,7 @@ function App(){
         <Food/>
         <Card/>
         <Button/>
+        <Student name="Patrick" age={16} isStudent={true}/>
         <Footer/>
       </>
     );
